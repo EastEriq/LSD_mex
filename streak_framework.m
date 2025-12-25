8750cabf84a5c1daa6bca5594434f14d2236d881
Enrico Segre <enrico.segre@weizmann.ac.il>
2025-12-25 16:43:50 +0200

    WIP streak framework

--- streak_framework.m
+++ streak_framework.m
@@ -1,49 +1,63 @@
 testplot=true;
 nimages=5;
 nimplants=100;
+maxstreaks=3;
+
+imagepath='/home/enrico/Eran/testimages/';
+psffiles=dir(fullfile(imagepath,'*sci_coadd_PSF_1.fits'));
+imagefiles=dir(fullfile(imagepath,'*sci_coadd_Image_1.fits'));
 
 % create a table for storing results
-detectiontable=table;
+detectiontable=table('Size',[nimages*nimplants,5],...
+    'VariableTypes',{'string','string','struct','struct','double'},...
+    'VariableNames',{'folder','image','implanted','detected','timing'});
+
 
 for i=1:nimages
     % pick up an image in the image directory and its corresponding PSF
     % (assume that both files are present, so it is sufficient to check for
     %  the existence of one of them)
-    imagepath='/home/enrico/Eran/testimages/';
-    
-    AI=AstroImage('/home/enrico/Eran/testimages/LAST.01.02.01_20251203.030427.430_clear_1423_000_001_014_sci_coadd_Image_1.fits');
-    
-    PSF=AstroImage('/home/enrico/Eran/testimages/LAST.01.02.01_20251203.030427.430_clear_1423_000_001_014_sci_coadd_PSF_1.fits');
+    ifile=floor(rand*numel(imagefiles))+1;
+    AI=AstroImage(fullfile(imagefiles(ifile).folder,imagefiles(ifile).name));
+    PSF=AstroImage(fullfile(psffiles(ifile).folder,psffiles(ifile).name));
     AI.PSF=PSF.Image;
     
     % normalize the image
-    
     AI=imProc.background.backVar(AI,'Block',[128 128], 'Method',...
         @imUtil.background.modeVar_LogHist, 'MethodArgs',{{'MinVal',5, 'MaxVal',3000},{}});
     AI=imProc.image.subBackDivideStd(AI);
     
     % implant random streaks
     for j=1:nimplants
-        nstreaks=floor(rand*4);
-        streakcoords = rand(nstreaks,4)*2*size(AI.Image,1);
-        streakstrength = rand*1000; % maybe better log distribution
+        AI1=AI.copy;
+        nstreaks=floor(rand*maxstreaks+1);
+        streakcoords = [(rand(nstreaks,2)-0.25)*2*size(AI1.Image,1),...
+                        (rand(nstreaks,2)-0.25)*2*size(AI1.Image,2)];
+        streakstrength = rand(nstreaks,1)*1000; % maybe better log distribution
         for k=1:nstreaks
-            AI.ImageData.Image = imUtil.streaks.addLineToImage(AI.ImageData.Image, streakcoords(k,:),...
+            AI1.ImageData.Image = imUtil.streaks.addLineToImage(AI1.ImageData.Image, streakcoords(k,:),...
                 streakstrength(k), AI.PSFData.Data);
         end
         
-        AI=imProc.image.xcorrWithPSF(AI);
+        % refilter with PSF (matched filtering?)
+        AI1=imProc.image.xcorrWithPSF(AI1);
           
-        im=(AI.Image);
+        im=(AI1.Image);
         
         % detect streaks
         tic;
         segs=lsd_scale_mex(im,1/3);
-        toc
-        
         segs=merge_segments(segs);
+
+        detectiontable.timing(l) = toc;
         
         % store the test results in the table
+        l=(i-1)*nimages+j;
+        detectiontable.folder(l) = imagefiles(ifile).folder;
+        detectiontable.image(l) = imagefiles(ifile).name;
+        detectiontable.implanted(l).coords = streakcoords(:,[1 3 2 4]);
+        detectiontable.implanted(l).strength = streakstrength;
+        detectiontable.detected(l).coords = segs';
         
         % plot for visual feedback
         if testplot
@@ -52,6 +66,7 @@ for i=1:nimages
             hold on
             plot(segs([2,4],:),segs([1,3],:),'LineWidth',2)
             hold off
+            drawnow
         end
         
     end
